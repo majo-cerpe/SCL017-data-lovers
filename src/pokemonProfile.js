9c655d388e71cacbef4288525e794af1915710f6
majo-cerpe <mariajose_cerpe@outlook.com>
2021-06-30 17:12:22 -0400

    cambio if x switch y capitalize títulos

--- src/pokemonProfile.js
+++ src/pokemonProfile.js
@@ -7,6 +7,7 @@ console.log(data);
 
 const containerRoot = document.getElementById('root');
 let htmlCode = ''
+
     ,clickedPokemonId = sessionStorage.getItem("clickedPokemonId")
     ,selectedPokemonIndex = clickedPokemonId -1
     ,nextPokemonIndex = 0
@@ -32,7 +33,7 @@ switch(clickedPokemonId){
         nextPokemonIndex = selectedPokemonIndex +1;
     htmlCode = 
         `<div class=nextPokemon>
-            <button class=nextPokemonButton>${data.pokemon[nextPokemonIndex].name + "#" + data.pokemon[nextPokemonIndex].num}</button>
+            <button class=nextPokemonButton>${data.pokemon[nextPokemonIndex].name + " #" + data.pokemon[nextPokemonIndex].num}</button>
         </div>`
     break;
 
@@ -40,7 +41,7 @@ switch(clickedPokemonId){
         previousPokemonIndex = selectedPokemonIndex -1;
     htmlCode = 
         `<div class=prevPokemon>
-           <button class=prevPokemonButton>${data.pokemon[previousPokemonIndex].name + "#" + data.pokemon[previousPokemonIndex].num}</button>
+           <button class=prevPokemonButton>${data.pokemon[previousPokemonIndex].name + " #" + data.pokemon[previousPokemonIndex].num}</button>
         </div>`
     break;
 
@@ -49,18 +50,18 @@ switch(clickedPokemonId){
         previousPokemonIndex = selectedPokemonIndex -1;
     htmlCode = 
         `<div class=prevNextPokemon>
-            <button class=prevPokemonButton>${data.pokemon[previousPokemonIndex].name + "#" + data.pokemon[previousPokemonIndex].num}</button>
-            <button class=nextPokemonButton>${data.pokemon[nextPokemonIndex].name + "#" + data.pokemon[nextPokemonIndex].num}</button>
+            <button class=prevPokemonButton>${data.pokemon[previousPokemonIndex].name + " #" + data.pokemon[previousPokemonIndex].num}</button>
+            <button class=nextPokemonButton>${data.pokemon[nextPokemonIndex].name + " #" + data.pokemon[nextPokemonIndex].num}</button>
         </div>`
 }
 
-htmlCode += `<h1>${data.pokemon[selectedPokemonIndex].name}</h1>
+htmlCode += `<h1>${data.pokemon[selectedPokemonIndex].name + " #" + data.pokemon[selectedPokemonIndex].num}</h1>
             <figure>
                 <img src=${data.pokemon[selectedPokemonIndex].largeImg}>
             </figure>
             <section class=aboutSize>
                 <section>
-                    <h2>${"Sobre " + data.pokemon[selectedPokemonIndex].name}</h2>
+                    <h2 class=aboutTitle >${"Sobre " + data.pokemon[selectedPokemonIndex].name}</h2>
                     <div class = about>${data.pokemon[selectedPokemonIndex].about}</div>
                 </section>
                 <section>
@@ -112,13 +113,15 @@ data.pokemon[selectedPokemonIndex].weaknesses.forEach(weaknesses =>{
                 </section>
                 <section class = evolution>
                     <h2>${"Evoluciones y costo en caramelos"}</h2>`         
-    if (prevEvolution == false && data.pokemon[selectedPokemonIndex].evolution.nextEvolution.length <=1 && data.pokemon[selectedPokemonIndex].evolution.nextEvolution[0].hasOwnProperty('nextEvolution') == true){
+
+switch (true) {
+    case prevEvolution == false && data.pokemon[selectedPokemonIndex].evolution.nextEvolution.length <=1 && data.pokemon[selectedPokemonIndex].evolution.nextEvolution[0].hasOwnProperty('nextEvolution') == true:
         //caso 1: 1° pokemon de una familia con una evolución con una evolución (evolución lineal e.g. Bulbasaur) 
-                // o primer pokemon de una familia una evolución con 2 o más evoluciones (evolución mixta e.g. Poliwag)
+            // o primer pokemon de una familia una evolución con 2 o más evoluciones (evolución mixta e.g. Poliwag)
         nextEvolutionName = data.pokemon[selectedPokemonIndex].evolution.nextEvolution[0].name
         nextEvolutionCandyCost = data.pokemon[selectedPokemonIndex].evolution.nextEvolution[0].candyCost
         nextNextEvolutionCandyCost = data.pokemon[selectedPokemonIndex].evolution.nextEvolution[0].nextEvolution[0].candyCost
-    
+
         htmlCode += `<div class=evolutionCandy>
                         <h3>${selectedPokemonName}</h3>
                         <figure>
@@ -136,16 +139,15 @@ data.pokemon[selectedPokemonIndex].weaknesses.forEach(weaknesses =>{
             htmlCode += `</div>
                     </div>
                 </section>`
-    }
+        break;
 
-    if(prevEvolution == true && nextEvolution == true){
+    case prevEvolution == true && nextEvolution == true:    
         //caso 2: 2° pokemon de una familia de evolucion lineal (e.g. Ivysaur) o evolución mixta (e.g. Gloom)
         prevEvolutionName = data.pokemon[selectedPokemonIndex].evolution.prevEvolution[0].name
         prevEvolutionCandyCost = data.pokemon[selectedPokemonIndex].evolution.prevEvolution[0].candyCost
         nextEvolutionName = data.pokemon[selectedPokemonIndex].evolution.nextEvolution[0].name
         nextEvolutionCandyCost = data.pokemon[selectedPokemonIndex].evolution.nextEvolution[0].candyCost
-
-         htmlCode += `<div style="display: flex">
+        htmlCode += `<div style="display: flex">
                         <h3>${prevEvolutionName}</h3>
                         <figure>
                             <img src="img/arrowpoint.png" style="width: 100px; border: none"> 
@@ -160,17 +162,16 @@ data.pokemon[selectedPokemonIndex].weaknesses.forEach(weaknesses =>{
         data.pokemon[selectedPokemonIndex].evolution.nextEvolution.forEach(nextEvolution => {
                 htmlCode += `<h3>${nextEvolution.name}</h3>`}); 
             htmlCode += `</div>
-                     </div>
-                 </section>`
-    }
+                    </div>
+                </section>`    
+        break;
 
-    if (prevEvolution == true && nextEvolution == false && data.pokemon[selectedPokemonIndex].evolution.prevEvolution[0].hasOwnProperty('prevEvolution')== true){
+    case prevEvolution == true && nextEvolution == false && data.pokemon[selectedPokemonIndex].evolution.prevEvolution[0].hasOwnProperty('prevEvolution')== true:
         //caso 3: 3° pokemon de una familia con una pre evolución con una pre evolucion (e.g. Venusaur)
         prevEvolutionName = data.pokemon[selectedPokemonIndex].evolution.prevEvolution[0].name
         prevEvolutionCandyCost = data.pokemon[selectedPokemonIndex].evolution.prevEvolution[0].candyCost
         prevPrevEvolutionName = data.pokemon[selectedPokemonIndex].evolution.prevEvolution[0].prevEvolution[0].name
         prevPrevEvolutionCandyCost = data.pokemon[selectedPokemonIndex].evolution.prevEvolution[0].prevEvolution[0].candyCost
-
         htmlCode += `<div style="display: flex">
                         <h3>${prevPrevEvolutionName}</h3>
                         <figure>
@@ -185,13 +186,12 @@ data.pokemon[selectedPokemonIndex].weaknesses.forEach(weaknesses =>{
                         <h3>${selectedPokemonName}</h3>
                     </div>
                 </section>`
-    }
-
-    if(prevEvolution == false && data.pokemon[selectedPokemonIndex].evolution.nextEvolution.length >= 2){
+        break;
+    
+    case prevEvolution == false && data.pokemon[selectedPokemonIndex].evolution.nextEvolution.length >= 2:
         //caso 4: 1° pokemon de una familia con evolución ramificada (e.g. Eevee)
         nextEvolutionName = data.pokemon[selectedPokemonIndex].evolution.nextEvolution[0].name
         nextEvolutionCandyCost = data.pokemon[selectedPokemonIndex].evolution.nextEvolution[0].candyCost
-
         htmlCode += `<div style="display: flex">
                         <h3>${selectedPokemonName}</h3>
                         <figure>
@@ -204,13 +204,12 @@ data.pokemon[selectedPokemonIndex].weaknesses.forEach(weaknesses =>{
             htmlCode += `</div>
                     </div>
                 </section>`
-    }
+        break;
 
-    if (prevEvolution == false && data.pokemon[selectedPokemonIndex].evolution.nextEvolution.length <=1 && data.pokemon[selectedPokemonIndex].evolution.nextEvolution[0].hasOwnProperty('nextEvolution') == false){
+    case prevEvolution == false && data.pokemon[selectedPokemonIndex].evolution.nextEvolution.length <=1 && data.pokemon[selectedPokemonIndex].evolution.nextEvolution[0].hasOwnProperty('nextEvolution') == false: 
         //caso 5: 1° pokemon de una familia con una evolución lineal e.g. Rattata) 
         nextEvolutionName = data.pokemon[selectedPokemonIndex].evolution.nextEvolution[0].name
         nextEvolutionCandyCost = data.pokemon[selectedPokemonIndex].evolution.nextEvolution[0].candyCost
-
         htmlCode += `<div style="display: flex">
                         <h3>${selectedPokemonName}</h3>
                         <figure>
@@ -220,11 +219,11 @@ data.pokemon[selectedPokemonIndex].weaknesses.forEach(weaknesses =>{
                         <h3>${nextEvolutionName}</h3>
                     </div>
                 </section>`
-    }
+        break;
 
-     if (prevEvolution == true && nextEvolution == false && data.pokemon[selectedPokemonIndex].evolution.prevEvolution[0].hasOwnProperty('prevEvolution')== false){
+    case prevEvolution == true && nextEvolution == false && data.pokemon[selectedPokemonIndex].evolution.prevEvolution[0].hasOwnProperty('prevEvolution')== false:
         //caso 6 : 2° pokemon de una familia de evolucion lineal sin evoluciones (e.g. Raticatte)
-                // o segundos pokemones de una familia de evolución ramificada (e.g. Voporeon, Slowbro, Hitmontop) 
+            // o segundos pokemones de una familia de evolución ramificada (e.g. Voporeon, Slowbro, Hitmontop) 
         prevEvolutionName = data.pokemon[selectedPokemonIndex].evolution.prevEvolution[0].name
         prevEvolutionCandyCost = data.pokemon[selectedPokemonIndex].evolution.prevEvolution[0].candyCost
         htmlCode += `<div style="display: flex">
@@ -236,9 +235,9 @@ data.pokemon[selectedPokemonIndex].weaknesses.forEach(weaknesses =>{
                         <h3>${selectedPokemonName}</h3>
                     </div>
                 </section>`
-     }
+        break;
+}
         `</section>
     </section>`
-     
-
+ 
 containerRoot.innerHTML = htmlCode
