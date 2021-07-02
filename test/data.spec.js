4c866520a3ddab2b8ae20f6ce0c84fd62429309d
majo-cerpe <mariajose_cerpe@outlook.com>
2021-07-02 12:37:30 -0400

    intentos de test

--- test/data.spec.js
+++ test/data.spec.js
@@ -11,37 +11,32 @@ describe('operation', () => {
       expect(typeof operation.search).toBe('function');
     });
 
- /* it.skip('debería retornar true para "4083952015263"', () => {
-      // escribe aquí tu test
+    describe("nombre pokemon ingresado por usuario", () => {
+      const pokemonName = "pikachu"
+      const pokemonNum =  "025"
+      
+      test("comprobar que el nombre corresponde con el numero")
+      expect(pokemonName).(pokemonNum)
     });
-
-    it.skip('debería retornar true para "79927398713"', () => {
-      // escribe aquí tu test
-    });
-
-    it.skip('debería retornar false para "1234567890"', () => {
-      // escribe aquí tu test
-    }); */
   });
+
+
+ /*  describe('operation.search.searchOrClickedPokemonId', () => {
+    it('should be 025 for pikachu search', () => {
+      const searchPokemonByName = 'pikachu'
+      expect('operation.search.searchOrClickedPokemonId').toBe('025');
+    });
+  }); */
+
+
 
 
   describe('operation.clickedPokemonProfileDisplay', () => {
     it('should be an function', () => {
       expect(typeof operation.clickedPokemonProfileDisplay).toBe('function');
     });
-
-  /*it.skip('Debería retornar "############5616" para "4556364607935616"', () => {
-      // escribe aquí tu test
-    });
-
-    it.skip('Debería retornar "1" para "1"', () => {
-      // escribe aquí tu test
-    });
-
-    it.skip('Debería retornar "######orld" para "helloworld"', () => {
-      // escribe aquí tu test
-    }); */
   });
+
 });
 
 
